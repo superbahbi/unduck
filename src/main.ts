62866d1d174f88d0ee5a0035d77e60aa23a780e6
Bobby Lee <superbahbi@gmail.com>
2025-02-27 11:31:42 -0800

    Change link

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ function noSearchDefaultPageRender() {
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="https://unduck.robertkugler.dev?q=%s"
             readonly 
           />
           <button class="copy-button">
@@ -20,13 +20,6 @@ function noSearchDefaultPageRender() {
           </button>
         </div>
       </div>
-      <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
-        •
-        <a href="https://x.com/theo" target="_blank">theo</a>
-        •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
-      </footer>
     </div>
   `;
 
